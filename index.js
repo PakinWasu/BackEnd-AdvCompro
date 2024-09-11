1041d71734ebdb1c187808ad2ed163945c44eece
Pakin Wasu <pakin.wasu2002@gmail.com>
2024-09-11 10:05:38 +0700

    cs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-const dbUrl = "";
+const dbUrl = "postgres://webadmin:DHPxmf57129@node65666-pakin-app.proen.app.ruk-com.cloud:11558/Books";
 const sequelize = new Sequelize(dbUrl);
 const Book = sequelize.define("book", {
   id: {
